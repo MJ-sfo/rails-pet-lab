3b88aedb51e18cbd3c97347f083c55b432dc2122
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-23 11:47:25 -0700

    wrote validations name and email

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -6,6 +6,13 @@ class Owner < ActiveRecord::Base
 
   # TODO: add validations
 
+  class Person < ApplicationRecord
+    validates :first_name, presence: true, length: { minimum: 255 }
+    validates :last_name, presence: true, length: { minimum: 255 }
+    validates :email, presence: true, length: { minimum: 255 }, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,
+    message: "email must include '@' sign" }
+  end
+
   before_save :normalize_phone_number
 
   # removes leading 1 and the characters (, ), -, .
